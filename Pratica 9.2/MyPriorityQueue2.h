11540c42c8430fa92ae92d48a879439e6a1e0a9e
Larissa Tonetto <larissatonetto0@gmail.com>
2020-11-15 13:10:51 -0200

    Etapa 1 terminada

--- Pratica 9.2/MyPriorityQueue2.h
+++ Pratica 9.2/MyPriorityQueue2.h
@@ -25,24 +25,48 @@ private:
 
 //ATENCAO: propositalmente nao temos funcoes constantes, visto que nao implementamos iteradores
 // constantes na nossa classe lista...
-template<class T>
+template<class T>	// OK
 void MyPriorityQueue2<T>::push(const T&el) {
-
+	elementos.push_back(el);
 }
 
-template<class T>
+template<class T>	// OK
 T & MyPriorityQueue2<T>::top() {
-	
+	typename MyList2<T>::iterator it1 = elementos.begin();	// Guarda o elemento que será retornado
+	typename MyList2<T>::iterator it2 = elementos.begin();	// Percorre a lista
+
+	while(it2 != elementos.end()) {
+		if (*it2 > *it1) it1 = it2;
+		it2++;
+	}
+
+	return *it1;
 }
 
 template<class T>
 void MyPriorityQueue2<T>::pop() {
-	
+	typename MyList2<T>::iterator it1 = elementos.begin();
+	typename MyList2<T>::iterator it2 = elementos.begin();
+
+	while(it2 != elementos.end()) {
+		if (*it2 > *it1) it1 = it2;
+		it2++;
+	}
+
+	elementos.erase(it1);
 }
 
 template<class T>
 int MyPriorityQueue2<T>::size() {
-	
+	int size = 0;
+	typename MyList2<T>::iterator it = elementos.begin();
+
+	while(it != elementos.end()) {
+		size++;
+		it++;
+	}
+
+	return size;
 }
 
 #endif
