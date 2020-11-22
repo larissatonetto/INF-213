cd9a75cc0ed04e8b27a5ad518bf3908a131fc13a
Larissa Tonetto <larissatonetto0@gmail.com>
2020-11-22 17:27:37 -0200

    Etapas 1 e 3 com alguns bugs

--- Prática 10/arvoreExpressao.cpp
+++ Prática 10/arvoreExpressao.cpp
@@ -20,6 +20,11 @@ private:
 	MyVec<int> filhoEsquerdo;
 	MyVec<int> filhoDireito;
 	MyVec<char> operador;
+
+	void imprimeExpressao(int i) const;		// Imprime a partir do nodo i
+	int avaliaValor(int i) const;
+	int altura(int i) const;
+	void nivelMaisNodos(int i, int nivel, int *largura) const;
 };
 
 void ArvoreExpressao::imprimeArrays() const {
@@ -36,23 +41,99 @@ void ArvoreExpressao::imprimeArrays() const {
 
 
 void ArvoreExpressao::leArvore() {
-	
+	MyVec<char> nodo;
+	int v, n;
+	char c;
+	cin >> v;
+
+	for (int i = 0; i < v; i++) {
+		// Lê primeiro em que nodo estou -> filho da esquerda -> filho da direita -> nodo
+		cin >> n;
+
+		cin >> n;
+		filhoEsquerdo.push_back(n);
+
+		cin >> n;
+		filhoDireito.push_back(n);
+
+		cin >> c;
+		operador.push_back(c);
+	}
+}
+
+void ArvoreExpressao::imprimeExpressao(int i) const {
+	if (operador[i] == '+' || operador[i] == '-' || operador[i] == '*') {
+		imprimeExpressao(filhoEsquerdo[i]);
+		cout << operador[i];
+		imprimeExpressao(filhoDireito[i]);
+	} else {
+		if (i%2 == 0) {
+			cout << operador[i] << ")";
+		} else {
+			cout << "(" << operador[i];
+		}
+	}
 }
 
 void ArvoreExpressao::imprimeExpressao() const {
-	
+	// O filho esquerdo de op[i] é fEsq[i] = op[fEsq[i]]
+	// O filho direito de op[i] é fDir[i] = op[fDir[i]]
+	// Um operador sempre tem dois filhos
+
+	imprimeExpressao(0);
+}
+
+int ArvoreExpressao::avaliaValor(int i) const {
+	if (operador[i] == '+' || operador[i] == '-' || operador[i] == '*') {
+		if (operador[i] == '+') {
+			return (avaliaValor(filhoEsquerdo[i]) + avaliaValor(filhoDireito[i]));
+		} else if (operador[i] == '-') {
+			return (avaliaValor(filhoEsquerdo[i]) - avaliaValor(filhoDireito[i]));
+		} else {
+			return (avaliaValor(filhoEsquerdo[i]) * avaliaValor(filhoDireito[i]));
+		}
+		
+	} else {
+		return operador[i];
+	}
 }
 
 int ArvoreExpressao::avaliaValor() const {
+	return (avaliaValor(0));
+}
+
+int ArvoreExpressao::altura(int i) const {
+	if (operador[i] != '+' && operador[i] != '-' && operador[i] != '*') return 0;
 	
+	return 1+max(altura(filhoEsquerdo[i]),altura(filhoDireito[i]));
 }
 
 int ArvoreExpressao::altura() const {
-	
+	if (operador.size() == 1) return 0;
+
+	return altura(0);
+}
+
+void ArvoreExpressao::nivelMaisNodos(int i, int nivel, int *largura) const {
+	largura[nivel]++;
+
+	if (operador[i] != '+' && operador[i] != '-' && operador[i] != '*') return;
+
+	nivelMaisNodos(filhoEsquerdo[i], nivel+1, largura);
+	nivelMaisNodos(filhoDireito[i], nivel+1, largura);
 }
 
 int ArvoreExpressao::nivelMaisNodos() const {
+	int max = 0, h = altura();
+	int largura[h] = {0};
 
+	nivelMaisNodos(0, 0, largura);
+
+	for (int i = 0; i < h; i++) {
+		if (largura[i] > max) max = largura[i];
+	}
+
+	return max;
 }
 
 
