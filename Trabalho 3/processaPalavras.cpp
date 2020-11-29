bb3970cb1fb15ffaf410c2a2764612a398fd1449
Larissa Tonetto <larissatonetto0@gmail.com>
2020-11-29 18:47:36 -0200

    Separação de sentenças funcionando

--- Trabalho 3/processaPalavras.cpp
+++ Trabalho 3/processaPalavras.cpp
@@ -10,52 +10,54 @@ int main(int argc, char **argv) {
     string line, teste;
     MyVec<string> frases;
     MyMap<string,int> map1;
-    int pos = 0;
+
+    // Lê a entrada a cada char e adiciona à string
+    // Se o char for um separador de sentença, a string atual é passada para MyVec e resetada
 
     if (argc == 2) {
         ifstream entrada(argv[1]);
         while(getline(entrada,line)) {
             
+            for (int i = 0; i < line.size(); i++) {    // TODO: Colocar em uma função?
+                // Convertendo maiúscula para minúscula
+                if (line[i] > 64 && line[i] < 91) line[i]+= 32;
+            }
+
+            teste+=line + " ";
+        }
+    } else {
+        cin >> line;    // Ignora o INICIO_TREINO
+        cin.ignore();
+        while (getline(cin,line)) {
+            if (line == "FINAL_TREINO") break;
+            
             for (int i = 0; i < line.size(); i++) {
                 // Convertendo maiúscula para minúscula
                 if (line[i] > 64 && line[i] < 91) line[i]+= 32;
             }
-            // if (line[line.size()-1] == '.' || line[line.size()-1] == ',') line.pop_back();
-            // Se eu chegar em um separador de sentença, mande a string atual para o MyVec e recomece
 
-            // map1[line] += 1;
             teste+=line + " ";
-            // cout << teste << "\n";
-        }
-    } else {
-        cout << "else\n";
-        while (getline(cin,line)) {
-            if (line == "FINAL_TREINO") break;
-            frases.push_back(line);
+            
         }   
     }
 
-    // cout << map1["sentenca"] << "\n";
-
-    // cout << "Frases:\n" << teste << "\n";
-    /*for (int i = 0; i < teste.size(); i++) {
-        char c = teste[i];
-        if (c > 64 && c < 91){
-            if (teste[i]+1)
-        } c+= 32;
-        s.push_back(c);
-    }
-
-    cout << s << "\n";*/
     string s;
-    // cout << teste << "\n";
+    cout << "teste = " << teste << "\n";
     for (int i = 0; i < teste.size(); i++) {
-        if (teste[i] == '.'   ||
-            teste[i] == '!'   ||
+        if (s[0] == ' ') s.erase(0,1);
+
+        if (teste[i] == 39) {
+            s+= ' ';
+            continue;
+        }
+
+        if (teste[i] == '!'   ||
             (teste[i] > 45 && teste[i] < 65) ||    // Retira de . até @
             (teste[i] > 34 && teste[i] < 39) ||
             (teste[i] > 39 && teste[i] < 45)){
-            frases.push_back(s);
+            if (s != "") {
+                frases.push_back(s);
+            }
             s = "";
             continue;
         }
@@ -63,6 +65,13 @@ int main(int argc, char **argv) {
         s+=teste[i];
     }
 
+    // Se ainda há uma sentença a ser guardada
+    if (s != "") {
+        if (s[0] == ' ') s.erase(0,1);
+        frases.push_back(s);
+    }
+
+    cout << frases.size() << "\n";
     for (int i = 0; i < frases.size(); i++) {
         cout << frases[i] << "\n";
     }
@@ -72,3 +81,5 @@ int main(int argc, char **argv) {
 
 // Aspas, apóstrofo e newline separam palavras
 // Números e sinais de pontuação separam sentenças
+
+// cout << map1["sentenca"] << "\n";
